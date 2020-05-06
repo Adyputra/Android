84f8d908e2813341eee687959b42a5441e5abd96
adynugrahaputra <adynugrahaputra@gmail.com>
2020-05-07 03:31:52 +0700

    tambah login with google dan ganti password

--- SILAPER/app/src/main/java/com/example/silaper/profil/ProfilUser.java
+++ SILAPER/app/src/main/java/com/example/silaper/profil/ProfilUser.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -28,8 +30,9 @@ import org.json.JSONObject;
 
 public class ProfilUser extends AppCompatActivity {
 
-    TextView txnama, txemail;
-    RelativeLayout rtedit, rtkeluar;
+    String daftar_via="1";
+    TextView txnama, txemail, txtgantipass;
+    RelativeLayout rtedit, rtkeluar, rteditpass, rtopenwa;
     ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +42,28 @@ public class ProfilUser extends AppCompatActivity {
         progressDialog = new ProgressDialog(ProfilUser.this);
         txnama = findViewById(R.id.txnama);
         txemail = findViewById(R.id.txemail);
+        txtgantipass = findViewById(R.id.txt_ganti_pass);
         rtedit = findViewById(R.id.rteditakun);
+        rteditpass = findViewById(R.id.rtgantipassword);
+        rtopenwa = findViewById(R.id.rtopenwa);
 
         getdata();
 
+        if(daftar_via.equals("2")){
+            txtgantipass.setText("Ganti Password (Disable)");
+        }
+
+        rteditpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(daftar_via.equals("2")){
+                    Toast.makeText(ProfilUser.this, "Anda Tidak Dapat Ganti Password Karena Akun Anda Mendaftar Via Akun Google", Toast.LENGTH_SHORT).show();
+                }else{
+                    startActivity(new Intent(ProfilUser.this, GantiPassword.class));
+                }
+            }
+        });
         rtedit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,7 +72,24 @@ public class ProfilUser extends AppCompatActivity {
                 Intent intent = new Intent(ProfilUser.this, EditProfil.class);
                 intent.putExtra("nama",txnama.getText().toString());
                 intent.putExtra("email",txemail.getText().toString());
+                intent.putExtra("daftar_via",daftar_via);
                 startActivity(intent);
+            }
+        });
+        rtopenwa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String contact = "+6289696491481", pesan ="Hello Admin, Saya "+ txnama.getText().toString()+" dari aplikasi SILAPER";
+                String url = "https://api.whatsapp.com/send?phone=" + contact +"&text="+pesan;
+                try {
+                    PackageManager pm = getPackageManager();
+                    pm.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES);
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                } catch (PackageManager.NameNotFoundException e) {
+                    Toast.makeText(ProfilUser.this, "Whatsapp app not installed in your phone", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -75,7 +113,7 @@ public class ProfilUser extends AppCompatActivity {
                         JSONObject data = respon.getJSONObject("data");
                         txnama.setText(data.getString("nama"));
                         txemail.setText(data.getString("email"));
-
+                        daftar_via = data.getString("daftar_via");
                     }else {
                         Toast.makeText(ProfilUser.this, respon.getString("pesan"), Toast.LENGTH_SHORT).show();
                     }
