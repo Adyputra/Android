79a0ec18658f1248e2fec71d71099bfac7519c5e
Adyputra <adynugrahaputra@gmail.com>
2020-05-08 20:37:53 +0700

    Tambahan

--- SILAPER/app/src/main/java/com/example/silaper/configfile/ServerApi.java
+++ SILAPER/app/src/main/java/com/example/silaper/configfile/ServerApi.java
@@ -1,6 +1,6 @@
 package com.example.silaper.configfile;
 
 public class ServerApi {
-   public static final String IP="http://192.168.1.7/Mei/Project/Bersama/";
+   public static final String IP="http://192.168.1.7/project/Bersama/";
    public static final String IPServer= IP + "api/";
 }
